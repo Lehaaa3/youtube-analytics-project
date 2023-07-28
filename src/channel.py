e242a6568e22e89fdd69879267de1618d4440499
Алексей Грабовский <leha.grabovskiy.00@bk.ru>
2023-07-29 01:53:44 +0300

    2nd homework - solved

--- src/channel.py
+++ src/channel.py
@@ -2,6 +2,9 @@ import os
 import json
 from googleapiclient.discovery import build
 
+api_key: str = os.getenv('API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -9,10 +12,27 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        self.channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.id = self.channel['items'][0]['id']
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']["default"]['url']
+        self.subscriber_count = self.channel['items'][0]["statistics"]['subscriberCount']
+        self.video_count = self.channel['items'][0]["statistics"]['videoCount']
+        self.view_count = self.channel['items'][0]["statistics"]['viewCount']
+
+    @classmethod
+    def get_service(cls):
+        """Получает объект для работы с YouTube API"""
+        api_key: str = os.getenv('API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, file_name):
+        """Записывает аттрибуты экземпляра класса в json файл"""
+        with open(file_name, 'w') as f:
+            json.dump(self.__dict__, f)
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        api_key: str = os.getenv('API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
