c2d7a3f46032df811ded48eca6c8f76199f2cd25
Алексей Грабовский <leha.grabovskiy.00@bk.ru>
2023-08-22 11:54:10 +0300

    6th homework - solved

--- src/video.py
+++ src/video.py
@@ -7,13 +7,20 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Video:
     def __init__(self, video_id: str):
-        self.video_id = video_id
-        self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                           id=self.video_id).execute()
-        self.url: str = self.video['items'][0]['snippet']['thumbnails']["default"]['url']
-        self.video_title: str = self.video['items'][0]['snippet']['title']
-        self.view_count: int = self.video['items'][0]['statistics']['viewCount']
-        self.like_count: int = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.video_id = video_id
+            self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=self.video_id).execute()
+            self.url: str = self.video['items'][0]['snippet']['thumbnails']["default"]['url']
+            self.video_title: str = self.video['items'][0]['snippet']['title']
+            self.view_count: int = self.video['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            self.video_id = video_id
+            self.url: str = None
+            self.title: str = None
+            self.view_count: int = None
+            self.like_count: int = None
 
     def __str__(self):
         return self.video_title
